86c92adaa9addffa2bd942df8f49eb1aa5d87142
Marie Vielmas <marie.vielmas@orange.fr>
2026-03-16 20:07:40 +0100

    totalité des fichiers

---
+++ app/routes/test.py
@@ -0,0 +1,9 @@
+from ..app import app, db
+from sqlalchemy import text  # ← ajoutez cet import
+
+@app.route("/test")
+def test():
+    result = db.session.execute(text('SELECT * FROM bird_detection')).fetchall() 
+    print(result)
+    return str(result)
+    
